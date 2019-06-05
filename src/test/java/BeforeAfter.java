b090c6c76640474fa6b023d05076f7214cfec43d
LenousikK <liloo-19@yandex.ru>
2019-06-06 01:13:24 +0300

    Updated BeforeAfter class: RemoteWebDriver is in use.

--- src/test/java/BeforeAfter.java
+++ src/test/java/BeforeAfter.java
@@ -1,17 +1,22 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BeforeAfter {
     protected WebDriver driver;
     protected WebDriverWait wait;
+    public static final String URL_TO_HUB = "http://localhost:4444/wd/hub";
 
     @BeforeEach
-    public void openBrowser() {
-        driver = new ChromeDriver();
+    public void openBrowser() throws MalformedURLException {
+        DesiredCapabilities capability = DesiredCapabilities.chrome();
+        driver = new RemoteWebDriver(new URL(URL_TO_HUB), capability);
         wait = new WebDriverWait(driver, 30);
     }
 
